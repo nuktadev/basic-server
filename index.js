89475196e63eeffd09b940beb169a8caf48ceac2
nuktadev <nukta.dev@gamil.com>
2024-06-29 18:55:02 +0600

    docker udpated

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World for dockers"));
+app.get("/", (req, res) => res.send("Hello World updated dockers"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
